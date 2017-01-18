fa5296243de255bc92bdb26c6b9e56d67f3f9719
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2017-01-18 15:06:10 +0000

    -Fixed unit test. -Minor release as 2.1.0 adding % duplication and estimated library  complexity stats to report.
    git-svn-id: https://svn.broadinstitute.org/BTL/MdReport@1442 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/test/scala/ReportersSpec.scala
+++ src/test/scala/ReportersSpec.scala
@@ -12,7 +12,6 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 import org.broadinstitute.MD.types.marshallers.Marshallers._
 import org.broadinstitute.mdreport.ReporterTraits.Requester
-import scala.collection.mutable
 import org.broadinstitute.mdreport.Reporters
 
 /**
@@ -121,74 +120,7 @@ class ReportersSpec extends FlatSpec with Matchers{
     val query = ssr.doQuery(mq)
     val result = query.flatMap(response => Unmarshal(response.entity).to[List[SampleMetrics]])
     val response = Await.result(result, 5 seconds)
-    val smartseq_map: mutable.LinkedHashMap[String, Any] = mutable.LinkedHashMap(
-      "sampleName" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.totalReads" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.meanReadLength" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pfReads" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pctPfReads" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pfReadsAligned" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pctPfReadsAligned" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.readsAlignedInPairs" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pctReadsAlignedInPairs" -> None,
-      "PicardAlignmentSummaryAnalysis.PicardAlignmentSummaryMetrics.pctAdapter" -> None,
-      "PicardMeanQualByCycle.r1MeanQual" -> None,
-      "PicardMeanQualByCycle.r2MeanQual" -> None,
-      "PicardInsertSizeMetrics.pairOrientation" -> None,
-      "PicardInsertSizeMetrics.meanInsertSize" -> None,
-      "PicardInsertSizeMetrics.medianInsertSize" -> None,
-      "PicardInsertSizeMetrics.standardDeviation" -> None,
-      "PicardReadGcMetrics.meanGcContent" -> None,
-      "ErccStats.totalErccReads" -> None,
-      "ErccStats.fractionErccReads" -> None,
-      "ErccStats.fractionGenomeReferenceReads" -> None,
-      "ErccStats.totalUnalignedReads" -> None,
-      "ErccStats.fractionUnalignedReads" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.fragmentLengthMean" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.chimericPairs" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.readLength" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.estimatedLibrarySize" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.fragmentLengthStdDev" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.expressionProfilingEfficiency" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.unpairedReads" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.baseMismatchRate" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.transcriptsDetected" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.totalPurityFilteredReadsSequenced" -> None,
-      "RnaSeqQcStats.readMetrics.ReadMetrics.failedVendorQCCheck" -> None,
-      "RnaSeqQcStats.covMetrics.CovMetrics.meanPerBaseCov" -> None,
-      "RnaSeqQcStats.covMetrics.CovMetrics.meanCV" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.mapped" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.mappedPairs" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.alternativeAlignments" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.uniqueRateofMapped" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.mappingRate" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.mappedUnique" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.end1MappingRate" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.end2MappingRate" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.mappedUniqueRateofTotal" -> None,
-      "RnaSeqQcStats.alignmentMetrics.AlignmentMetrics.duplicationRateOfMapped" -> None,
-      "RnaSeqQcStats.gapMetrics.GapMetrics.cumulGapLength" -> None,
-      "RnaSeqQcStats.gapMetrics.GapMetrics.gapPct" -> None,
-      "RnaSeqQcStats.gapMetrics.GapMetrics.numGaps" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.intronicRate" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.rRNA" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.genesDetected" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.rRNArate" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.exonicRate" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.intragenicRate" -> None,
-      "RnaSeqQcStats.annotationMetrics.AnnotationMetrics.intergenicRate" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end1PctSense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end1Antisense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.noCovered5Prime" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end2MismatchRate" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end2Antisense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end2PctSense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end2Sense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end1MismatchRate" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.end1Sense" -> None,
-      "RnaSeqQcStats.endMetrics.EndMetrics.fivePrimeNorm" -> None,
-      "RnaSeqQcStats.Notes" -> None
-    )
+    val smartseq_map = new SmartSeqReporter(config).smartseqMap
     val myMap = ssr.fillMap(smartseq_map, response)
     ssr.writeMaps(myMap, "C:\\Dev\\Scala\\MdReport\\", config.setId.get, config.version.get)
     class getSamples extends Requester {
@@ -224,7 +156,7 @@ class ReportersSpec extends FlatSpec with Matchers{
     val rootPath = "http://btllims.broadinstitute.org"
     val port = 9101
     val server = s"$rootPath:$port/MD"
-    val samples = Reporters.getSamples("Mouse-Nigrovic", Some(1483633919195L), server)
-    samples.length should be (96)
+    val samples = Reporters.getSamples("SSF-2119", Some(1484340935777L), server)
+    samples.length should be (24)
   }
 }
